43dc864a84a9bfb4d740a07fe5de4e224baa3cc8
erik Tapia <20203tn152@utez.edu.mx>
2023-10-19 15:48:31 -0600

    pinche kevin

--- lib/modules/home/adapters/screens/about.dart
+++ lib/modules/home/adapters/screens/about.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/kernel/themes/Colors_app.dart';
 
 class About extends StatelessWidget {
   const About({super.key});
@@ -6,12 +7,28 @@ class About extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Acerca de nosotros"),
-      ),
-      body: const Center(
-        child: Text("Acerca de nosotros"),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("Acerca de nosotros"),
+        ),
+        body: Row(
+          children: [
+            Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(top: 32, left: 12),
+                  margin: const EdgeInsets.symmetric(vertical: 16),
+                  color: ColorsApp.primaryColor,
+                  child: const Text("Erik Tapia"),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(top: 32, left: 12),
+                  margin: const EdgeInsets.symmetric(vertical: 16),
+                  color: Colors.black26,
+                  child: const Text("Erik Tapia"),
+                ),
+              ],
+            ),
+          ],
+        ));
   }
 }
